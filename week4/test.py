b1c3a5643521ddcb5d21ef093aa6f624968cf986
Evan Vaughan <evan13@gmail.com>
2018-08-31 12:30:16 -0400

    open file

--- week4/test.py
+++ week4/test.py
@@ -1,51 +1,56 @@
-min_worked = dict()
-userids = []
+import json
 
-userid = int(input("what is your user ID?"))
-if userid == 0:
-    print(min_worked)
-else:
-    hours = int(input("how many hours did you work?:"))
-    if userid not in min_worked:
-        min_worked[userid] = hours
+the_file = "hours.json"
+
+def open_file(filename):
+    with open(filename, 'r') as fileobject:
+        opened_file = json.load(fileobject)
+
+min_worked = open_file(the_file)
+
+def input_hours():
+    userid = int(input("what is your user ID?"))
+    if userid == 0:
+        #for aaa,value in tesla_dictionary.items():
+        #    print(aaa, value)
+        print("Employee ID : Hours Worked")
+        for userid,hours in min_worked.items():
+            print(userid, hours)
     else:
-        min_worked[userid] = min_worked[userid] + hours
+        hours = int(input("how many hours did you work?:"))
+        if userid not in min_worked:
+            min_worked[userid] = hours
+        else:
+            min_worked[userid] = min_worked[userid] + hours
+        update_file(the_file)
+
+def update_file(filename, dictionary=min_worked):
+        with open(filename, 'w') as jsonfile:
+            json.dump(min_worked, jsonfile)
 
 print(min_worked)
 
+openfile("hours.json")
 
-"""
 
-min_worked = openfile("hours.json")
 
-else:
-#insert userID in dictionary
 
-if userid > 0:
-    #read file
-    if userid in min_worked == True:
-#update key
-    hours = input("How many minutes did you work today?:\n")
-    #update text file w/ employee ID & hours
-    for hours in filename:
 
-elif userid = 0:
-    # boss' employee ID, print all userIDs w/ hours
-    print('Employee ID: Minutes Worked')
-    for userID,hours in min_worked.items():
-        print(userID, hours)
-        print(min_worked)
+new_dictionary = {
+    'Name': tesla_dictionary['Name'],
+    'Symbol': tesla_dictionary['Symbol'],
+    'LastPrice': tesla_dictionary['LastPrice'],
+    'Volume': tesla_dictionary['Volume'],
+}
 
-else:
-    userid = int(input("put in a user ID that is a number 0 or greater:"))
+print(new_dictionary)
 
-    def write_json(filename, dictionary=example_dict):
+def write_dictionary(filename, dictionary=new_dictionary):
     with open(filename, 'w') as jsonfile:
         json.dump(dictionary, jsonfile)
 
-def openfile(filename):
-    with open(filename, 'r') as fileobject:
-        return json.load(fileobject)
+write_dictionary('tesla_summary_ev.json')
+
 
 
 # min_worked.get(userid, "default")
