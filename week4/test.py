9f2ec4cd51e1d44d08eae2bd1cf5df0ca1d8a5a2
Evan Vaughan <evan13@gmail.com>
2018-08-31 13:32:48 -0400

    open, input, started update file

--- week4/test.py
+++ week4/test.py
@@ -7,19 +7,28 @@ def open_file(filename):
     with open(filename, 'r') as fileobject:
         return min_worked
 
+def update_file(filename):
+    with open(filename, 'w') as fileobject:
+        return min_worked
+
 def input_hours(filename):
     userid = int(input("what is your user ID?"))
     if userid == 0:
         print("Employee ID : Hours Worked")
         for userid,hours in min_worked.items():
             print(userid, hours)
+#            quit()
     else:
         hours = int(input("how many hours did you work?:"))
         if userid not in min_worked:
             min_worked[userid] = hours
         else:
             min_worked[userid] = min_worked[userid] + hours
-
+        print(min_worked)
+    #update min_worked
+    
+    input_hours(min_worked)
+    update_file(the_file)
 
 open_file(the_file)
 input_hours(min_worked)
