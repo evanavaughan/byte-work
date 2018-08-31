811c3067a37b707c3f8e14e5d953ee3013c857f5
Evan Vaughan <evan13@gmail.com>
2018-08-31 13:18:25 -0400

    opens file, input hours work

--- week4/test.py
+++ week4/test.py
@@ -1,18 +1,15 @@
-import json
+#import json
 
-the_file = "hours.json"
+the_file = "hours.txt"
+min_worked = dict()
 
 def open_file(filename):
     with open(filename, 'r') as fileobject:
-        opened_file = json.load(fileobject)
+        return min_worked
 
-min_worked = open_file(the_file)
-
-def input_hours():
+def input_hours(filename):
     userid = int(input("what is your user ID?"))
     if userid == 0:
-        #for aaa,value in tesla_dictionary.items():
-        #    print(aaa, value)
         print("Employee ID : Hours Worked")
         for userid,hours in min_worked.items():
             print(userid, hours)
@@ -22,36 +19,9 @@ def input_hours():
             min_worked[userid] = hours
         else:
             min_worked[userid] = min_worked[userid] + hours
-        update_file(the_file)
-
-def update_file(filename, dictionary=min_worked):
-        with open(filename, 'w') as jsonfile:
-            json.dump(min_worked, jsonfile)
-
-print(min_worked)
-
-openfile("hours.json")
 
 
+open_file(the_file)
+input_hours(min_worked)
 
 
-
-new_dictionary = {
-    'Name': tesla_dictionary['Name'],
-    'Symbol': tesla_dictionary['Symbol'],
-    'LastPrice': tesla_dictionary['LastPrice'],
-    'Volume': tesla_dictionary['Volume'],
-}
-
-print(new_dictionary)
-
-def write_dictionary(filename, dictionary=new_dictionary):
-    with open(filename, 'w') as jsonfile:
-        json.dump(dictionary, jsonfile)
-
-write_dictionary('tesla_summary_ev.json')
-
-
-
-# min_worked.get(userid, "default")
-"""
